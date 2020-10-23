548819b808bf97d07e6c8db4f97d200c3e37e593
tommytrg <tomastrg22@gmail.com>
2020-10-23 18:30:52 +0200

    wip

---
+++ src/components/steps/RepeatedMnemonicsDisclaimer.vue
@@ -0,0 +1,62 @@
+<template>
+<div>
+ <NavigationCard
+    ref="navCard"
+    data-test="header-4"
+    class="wallet-seed-validation"
+    title="Confirm your seed phrase"
+    previous-text="Back"
+    next-text="Next"
+    :previous-step="previousStep"
+    :next-step="nextStep"
+  >
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacus erat, sodales at finibus quis, auctor vitae neque. Donec molestie suscipit mauris, scelerisque dictum eros egestas eget. Phasellus facilisis urna id venenatis interdum. Proin nec lorem a risus facilisis fringilla sit amet quis magna. Suspendisse vitae mollis mauris. Praesent dignissim vel tellus et lobortis. Proin cursus sapien a enim rhoncus condimentum.</p>
+  </NavigationCard>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'WalletAlreadyImported',
+  methods: {
+    nextStep() {
+      this.$router.push(`/ftu/wallet-description?import=true`)
+    },
+    previousStep() {
+      this.$router.push('/ftu/import-wallet')
+    }
+  }
+}
+</script>
+
+<style>
+@import '@/styles/theme.scss';
+@import '@/styles/_colors.scss';
+
+.content {
+  padding: 40px;
+}
+
+.seed {
+  align-items: center;
+  border: $input_big-border;
+  border-radius: $input_big-border-radius;
+  box-sizing: border-box;
+  color: $input_big-color;
+  display: inline-flex;
+  font-size: 22px;
+  line-break: auto;
+  line-height: 1.5em;
+  margin: 24px 0;
+  padding: 16px;
+  width: 100%;
+}
+
+.match-error {
+  color: $red-2;
+}
+
+.paragraph {
+  margin-top: 16px;
+}
+</style>
