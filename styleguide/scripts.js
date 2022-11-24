05f7bdb7065dbb1eb8b3678f7037441f52dcf3e9
tommy <tomastrg22@gmail.com>
2022-11-24 16:39:23 +0100

    remove console.logs clean

--- styleguide/scripts.js
+++ styleguide/scripts.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Element from 'element-ui'
-import '@/styles/element-variables.scss'
+// import '@/styles/element-variables.scss'
 import '@/fontAwesome.js'
 
 Vue.use(Element)
